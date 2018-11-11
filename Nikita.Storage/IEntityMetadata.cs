553c7daf5eecd1bca5433f88c1cc343015395498
Dustin Welden <DWelden@globys.com>
2018-11-10 20:09:45 -0800

    Added IEntityMetadata info interface

---
+++ Nikita.Storage/IEntityMetadata.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="IEntityMetadata.cs" author="Dustin R. Heart">
+// Copyright (c) 2018 Dustin R. Heart. All rights reserved.
+// </copyright>
+
+namespace Nikita.Storage
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="IEntityMetadata" />
+    /// </summary>
+    public interface IEntityMetadata
+    {
+        /// <summary>
+        /// Gets the Name
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the Type
+        /// </summary>
+        string Type { get; }
+
+        /// <summary>
+        /// Gets the Author
+        /// </summary>
+        string Author { get; }
+
+        /// <summary>
+        /// Gets the LastModified
+        /// </summary>
+        DateTime LastModified { get; }
+    }
+}
