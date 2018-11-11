3129f6871bb41a6d40dce32b1234d9d2ea469e1d
Dustin Welden <DWelden@globys.com>
2018-11-10 20:09:34 -0800

    Added IEntity storage interface

---
+++ Nikita.Storage/IEntity.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IEntity.cs" author="Dustin R. Heart">
+// Copyright (c) 2018 Dustin R. Heart. All rights reserved.
+// </copyright>
+
+namespace Nikita.Storage
+{
+    /// <summary>
+    /// Defines the <see cref="IEntity" />
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Gets or sets the Name
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets the Type
+        /// </summary>
+        string Type { get; }
+    }
+}
