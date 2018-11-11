fce50f235218a4a57a31f36f123c7bfce252166f
Dustin Welden <DWelden@globys.com>
2018-11-10 20:10:20 -0800

    Added Author entity class

---
+++ Nikita.Storage/Entities/Author.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="Author.cs" author="Dustin R. Heart">
+// Copyright (c) 2018 Dustin R. Heart. All rights reserved.
+// </copyright>
+
+namespace Nikita.Storage.Entities
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Author" />
+    /// </summary>
+    public abstract class Author : IEntity
+    {
+        /// <summary>
+        /// Gets or sets the Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the Type
+        /// </summary>
+        public string Type => "author";
+
+        /// <summary>
+        /// Gets or sets the Full Name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Author data
+        /// </summary>
+        public string AuthorData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the CreatedDate
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ModifiedDate
+        /// </summary>
+        public DateTime ModifiedDate { get; set; }
+    }
+}
