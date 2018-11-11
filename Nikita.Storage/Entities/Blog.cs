a8105efb88d84ff2104c220dd427c9e423e64e4a
Dustin Welden <DWelden@globys.com>
2018-11-10 20:10:28 -0800

    Added Blog entity class

---
+++ Nikita.Storage/Entities/Blog.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="Blog.cs" author="Dustin R. Heart">
+// Copyright (c) 2018 Dustin R. Heart. All rights reserved.
+// </copyright>
+
+namespace Nikita.Storage.Entities
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Blog" />
+    /// </summary>
+    public abstract class Blog : IEntity
+    {
+        /// <summary>
+        /// Gets or sets the Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the Type
+        /// </summary>
+        public string Type => "blog";
+
+        /// <summary>
+        /// Gets or sets the Author Name
+        /// </summary>
+        public string AuthorName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Editor Name
+        /// </summary>
+        public string EditorName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Title change data
+        /// </summary>
+        public dynamic TitleChange { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Content
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Content change data
+        /// </summary>
+        public dynamic ContentChange { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Changelog line
+        /// </summary>
+        public string ChangeLog { get; set; }
+
+        /// <summary>
+        /// Gets or sets the CreatedDate
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ModifiedDate
+        /// </summary>
+        public DateTime ModifiedDate { get; set; }
+    }
+}
