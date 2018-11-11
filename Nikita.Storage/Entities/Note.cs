efaf5a3da0b2f84e7af872c0417ad27c56a56ee6
Dustin Welden <DWelden@globys.com>
2018-11-10 20:10:34 -0800

    Added Note entity class

---
+++ Nikita.Storage/Entities/Note.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="Note.cs" author="Dustin R. Heart">
+// Copyright (c) 2018 Dustin R. Heart. All rights reserved.
+// </copyright>
+
+namespace Nikita.Storage.Entities
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Note" />
+    /// </summary>
+    public abstract class Note : IEntity
+    {
+        /// <summary>
+        /// Gets or sets the Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the Type
+        /// </summary>
+        public string Type => "note";
+
+        /// <summary>
+        /// Gets or sets the Author Name
+        /// </summary>
+        public string AuthorName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Content
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the CreatedDate
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+    }
+}
