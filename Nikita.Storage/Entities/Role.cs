0c1f422b3d9281b1f97db30ab3075711ff4047ad
Dustin Welden <DWelden@globys.com>
2018-11-10 20:10:39 -0800

    Added Role entity class

---
+++ Nikita.Storage/Entities/Role.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Role.cs" author="Dustin R. Heart">
+// Copyright (c) 2018 Dustin R. Heart. All rights reserved.
+// </copyright>
+
+namespace Nikita.Storage.Entities
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Role" />
+    /// </summary>
+    public abstract class Role : IEntity
+    {
+        /// <summary>
+        /// Gets or sets the Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the Type
+        /// </summary>
+        public string Type => "role";
+
+        /// <summary>
+        /// Gets or sets the CreatedDate
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ModifiedDate
+        /// </summary>
+        public DateTime ModifiedDate { get; set; }
+    }
+}
